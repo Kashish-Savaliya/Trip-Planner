96b765b77b10f47080abd724ccbda47ee5303dc3
Kashish <[kashishsavaliya10@gmail.com]>
2024-08-19 03:03:51 +0530

    commits

--- app/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import User, Place, Nearby_Place,State, hist_back
-
-# Register your models here.
-admin.site.register(User)
-admin.site.register(Place)
-admin.site.register(Nearby_Place)
-admin.site.register(State)
-admin.site.register(hist_back)
